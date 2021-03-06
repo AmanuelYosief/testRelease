426edab10ae37a315b58d73f8169483a66e0192f
AmanuelYosief <43088213+AmanuelYosief@users.noreply.github.com>
2021-03-06 05:18:02 +0000

    gravatar fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,11 +19,11 @@ const RESOURCES = {
 "favicon-16x16.png": "a41f4b0b8f609a2ee9595ddb8a3248d5",
 "favicon.ico": "b2032bbae9c8634dc1365a6c2bb18b72",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "8758d21c44d9cf604ea46755a95e6dd4",
-"/": "8758d21c44d9cf604ea46755a95e6dd4",
+"index.html": "6fcbd8307c944072a03805f4eea8e875",
+"/": "6fcbd8307c944072a03805f4eea8e875",
 "jsQR.js": "57acfde2e7d8e7eb44dff4460daac729",
 "kira_logo.png": "95c92c4a1e5acb6346666aec627f1144",
-"main.dart.js": "ae1f9fe1688d565c3ac42c5cee41e5a9",
+"main.dart.js": "68d19f889a0117f69cae9a1662a7e811",
 "manifest.json": "3123d1736e095bf2c9244a1d83678c29",
 "version.json": "6b5cbd243a71bed9a69206760de414f4"
 };
