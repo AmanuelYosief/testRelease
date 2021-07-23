87c15f2e443b7119a4f793daa4eaebcef62a1aad
Amanuel Yosief Mussie <43088213+AmanuelYosief@users.noreply.github.com>
2021-07-23 21:20:37 +0100

    test visualizer

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,30 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "d85d04f831cc5319de406ba6299120e9",
-"assets/assets/config.json": "2f1305a291c5b1a6391cff728e4c3fbe",
-"assets/assets/images/background.png": "9f9dee0127351159e158c4a357594c89",
-"assets/assets/images/cover.jpeg": "1a762f948495823a0472596cfcb15643",
-"assets/assets/images/key.png": "349f92f0e8a84a6b8c4fadfcad4212d9",
-"assets/assets/images/kira_logo.png": "95c92c4a1e5acb6346666aec627f1144",
-"assets/assets/images/kira_logo_gray.png": "c4a5c302931a71734c712b6acfb7f4f3",
-"assets/assets/images/kira_qr_logo.png": "9367f9aab18605db502fbd31abde94e5",
+  "assets/AssetManifest.json": "0e2c317031152ea3c9b475f85ae4f6b8",
+"assets/assets/addrbook.json": "1c5e5560cf6141f4ec1fc5591fdbfee6",
+"assets/assets/galaxy.png": "1c46fa10182c00d019cabcecfab7aaf0",
+"assets/assets/ipData.json": "393935cff6d5230d8aee92bd9824045b",
+"assets/assets/nodeList.json": "a9b9e0f36eb6abbd5d604fc2fc2f5281",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "35c38d7ad1a12f3fe5e5d09be3454c6b",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
-"assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
-"favicon-16x16.png": "a41f4b0b8f609a2ee9595ddb8a3248d5",
-"favicon.ico": "b2032bbae9c8634dc1365a6c2bb18b72",
+"assets/NOTICES": "d08fd330f169a0d16c89530049e2a4c2",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "bce5d5074b90dba97d0c6e36c23992b4",
-"/": "bce5d5074b90dba97d0c6e36c23992b4",
-"jsQR.js": "57acfde2e7d8e7eb44dff4460daac729",
-"kira_logo.png": "95c92c4a1e5acb6346666aec627f1144",
-"main.dart.js": "bce8b786b8d9ca4a39bcae907dcc51fc",
-"manifest.json": "3123d1736e095bf2c9244a1d83678c29",
-"version.json": "6b5cbd243a71bed9a69206760de414f4"
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"index.html": "3b263221fdda0c2c933685669afc1184",
+"/": "3b263221fdda0c2c933685669afc1184",
+"main.dart.js": "260672e484dddfff04e6ba9e9a8718e7",
+"manifest.json": "626f5a4cf4d03275fd8c198da2a371e8",
+"version.json": "312a371d1b0098ff7530bbf4345552e1"
 };
 
 // The application shell files that are downloaded before a service worker can
@@ -44,7 +37,7 @@ self.addEventListener("install", (event) => {
   return event.waitUntil(
     caches.open(TEMP).then((cache) => {
       return cache.addAll(
-        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+        CORE.map((value) => new Request(value + '?revision=' + RESOURCES[value], {'cache': 'reload'})));
     })
   );
 });
